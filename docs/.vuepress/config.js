34a9c0e4639c1ea5914d6e1574a9bbfedee413ca
djl57 <18367317878@163.com>
2019-01-11 16:39:09 +0800

    dist

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,19 @@
 module.exports = {
-  title: 'js funs', 
-  description: '记录js的方法和属性',
-  // base: '/myblog/'
+  title: '个人博客', 
+  // description: ' ',
+  themeConfig: {
+    nav: [
+      { text: '首页', link: '/' },
+      { text: 'js文档', link: '/jsdoc/' },
+      { text: 'css黑魔法', link: '/cssdoc/' },
+      { text: 'github', link: 'https://github.com/djl57/start-vuepress' },
+    ]
+  },
+  // themeConfig: {
+  //   sidebar: [
+  //     '/',
+  //     '/page-a',
+  //     ['/page-b', 'Explicit link text']
+  //   ]
+  // }
 }
